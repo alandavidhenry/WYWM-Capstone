70315b989f37eff224fde8490189e7966c8073d6
Alan Henry <alandavidhenry@gmail.com>
2023-11-01 08:52:33 +0000

    Initial commit

---
+++ src/app/data/data.js
@@ -0,0 +1,49 @@
+const data = {
+    products: [
+      {
+         id: '1',
+         name: 'Be Curious Shirt',
+         image: '../images/beCuriousShirt.png',
+         price: 50
+      },
+      {
+         id: '2',
+         name: 'Creator Shirt',
+         image: '../images/creatorShirt.png',
+         price: 35
+      },
+      {
+         id: '3',
+         name: 'Doer Shirt',
+         image: '../images/doerShirt.png',
+         price: 35
+      },
+      {
+         id: '4',
+         name: 'Translator Shirt',
+         image: '../images/translatorShirt.png',
+         price: 35
+      },
+      {
+         id: '5',
+         name: 'Pro Shirt',
+         image: '../images/proShirt.png',
+         price: 60
+      }
+    ],
+    books: [
+      {
+        id: '1',
+        name: 'Be Curious Book',
+        image: '../images/beCuriousBook.png',
+        price: 10
+      },
+      {
+        id: '2',
+        name: 'WYWM Book',
+        image: '../images/beCuriousBook.png',
+        price: 15
+      }
+    ]
+};
+export default data;
